ebdb612386bb1f828bcfd91c3a91a97125db23c4
sanlwin00 <kosanlwinoo@gmail.com>
2024-09-01 10:26:28 -0400

    Added pageNumber to Question model

--- Models/Question.cs
+++ Models/Question.cs
@@ -26,6 +26,7 @@ namespace MockTestApi.Models
     {
         public string Text { get; set; }
         public string Url { get; set; }
+        public int Page { get; set; }
         public string MaterialId { get; set; }
         public int Paragraph { get; set; }
     }
